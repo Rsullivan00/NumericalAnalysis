e03173305430cb832150b11b2b996ab1c60acc19
Rick Sullivan <rick@ricksullivan.net>
2015-02-26 23:15:36 -0800

    Hw6 done

--- Hw6/svd.py
+++ Hw6/svd.py
@@ -1,7 +1,12 @@
-import numpy 
-
+import numpy as np 
 a = [[0,1,1],[0,1,0],[1,1,0],[0,1,0],[1,0,1]]
-u,s,v = numpy.linalg.svd(a)
-print(u)
-print(s)
-print(v)
+u,s,v = np.linalg.svd(a)
+print('U:%s' % u)
+print('S:%s' % s)
+print('V:%s' % v)
+
+# Approximations
+u_2 = [[x] for x in u[:,0]]
+s_2 = s[0]
+v_2 = v[0]
+print('Approximation reconstruction:\n%s' % (np.multiply(np.multiply(u_2, s_2), v_2)))
